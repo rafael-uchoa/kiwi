a21cf77c96ad3765c503f2058ae1ecbbf93feef1
Rafael Uchoa <rafaeluchoa.1996@gmail.com>
2020-07-01 00:35:27 -0300

    Refactored backend with Prisma

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,12 @@
-import { createConnection } from 'typeorm';
 import express from 'express';
 import cors from 'cors';
-import 'reflect-metadata';
-
 import routes from './routes';
 
-(async () => {
-  await createConnection();
+const app = express();
+const port = 7777;
 
-  const app = express();
-  const port = 7777;
+app.use(express.json());
+app.use(cors());
+app.use(routes);
 
-  app.use(express.json());
-  app.use(cors());
-  app.use(routes);
-
-  app.listen(port, () => console.log(`Server listening on port ${port}`));
-})();
+app.listen(7777, () => console.log(`Server listening on port ${port}`));
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,11 +1,110 @@
-import { Router } from 'express';
-import ProductController from './controllers/ProductController';
+import { PrismaClient } from '@prisma/client';
+import { Router, Request, Response } from 'express';
 
+const prisma = new PrismaClient();
 const routes = Router();
 
-routes
-  .get('/api', ProductController.list)
-  .post('/api', ProductController.create)
-  .delete('/api/:id', ProductController.delete);
+// @desc		Get all products
+// @route		GET /api
+routes.get(
+  '/api',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const products = await prisma.product.findMany();
+
+      res.status(200).json(products);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+);
+
+// @desc		Get all products in a category
+// @route		GET /api/:category
+routes.get(
+  '/api/:category',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { category } = req.params;
+
+      const products = await prisma.product.findMany({
+        where: { category },
+      });
+
+      res.status(200).json(products);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+);
+
+// @desc		Create a product
+// @route		POST /api
+routes.post(
+  '/api',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { name, category, price } = req.body;
+
+      const product = await prisma.product.create({
+        data: {
+          name,
+          category,
+          price,
+        },
+      });
+
+      res.status(200).json(product);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+);
+
+// @desc		Update a product
+// @route		PUT /api/:id
+routes.put(
+  '/api/:id',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+      const { name, category, price } = req.body;
+
+      const product = await prisma.product.update({
+        where: { id: Number(id) },
+        data: {
+          name,
+          category,
+          price,
+        },
+      });
+
+      res.status(200).json(product);
+    } catch (e) {
+      res.status(500).json(e);
+    }
+  }
+);
+
+// @desc		Delete a product
+// @route		DELETE /api/:id
+routes.delete(
+  '/api/:id',
+  async (req: Request, res: Response): Promise<void> => {
+    try {
+      const { id } = req.params;
+
+      const product = await prisma.product.delete({
+        where: {
+          id: Number(id),
+        },
+      });
+
+      res.status(200).json(product);
+    } catch (e) {
+      res.status(404).json(e);
+    }
+  }
+);
 
 export default routes;
